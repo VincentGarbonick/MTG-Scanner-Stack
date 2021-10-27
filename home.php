b40ce1f919eaa104b1aedd637701ff0f83e291f0
Vincent Garbonick <vrg6@uakron.edu>
2021-10-27 15:16:17 -0400

    get rudimentary website shell

--- home.php
+++ home.php
@@ -1,11 +1,16 @@
 <html>
 <head> 
     <title> Home </title>
-    <link rel="home.css" href="styles.css">
+    <link rel="stylesheet" href="home.css" type="text/css">
 </head>
 <body> 
+    <div class="title">
+        <p class="title-text">YOUR TITLE HERE</p>
+    </div>
     <div class="center">
-        <p>Hello</p>
+        <input type="button" name="action" value="Show table">
+        <input type="button" name="action" value="Render table">
+        <input type="button" name="aciton" value="Show list view"> 
     </div>
 </body> 
 </html>
