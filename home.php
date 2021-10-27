cbffb6fbcea6f0fb3465bd382f13a85f52f295b4
Vincent Garbonick <vrg6@uakron.edu>
2021-10-27 15:01:42 -0400

    hi

--- home.php
+++ home.php
@@ -1,3 +1,11 @@
 <html>
-    
+<head> 
+    <title> Home </title>
+    <link rel="home.css" href="styles.css">
+</head>
+<body> 
+    <div class="center">
+        <p>Hello</p>
+    </div>
+</body> 
 </html>
