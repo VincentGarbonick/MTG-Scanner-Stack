c67150f4f28cdfd4b30ead700cea891b0316eab6
Vincent Garbonick <vrg6@uakron.edu>
2021-11-03 16:35:34 -0400

    get resizing working decently well

---
+++ table-viewer.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="home.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" type="image/jpg" href="origin-symbol.png"/>
+  <title>Viewer</title>
+</head>
+<body>
+    <div class="sidebar-left">
+        <a href="home.html">
+            <img class="sidebar-image" src="origin-symbol.png">
+        </a>
+        <button class="btn-hover color-2 center">Visual Spoiler</button>
+    </div>
+
+    
+    <div class="sidebar-right">
+        <p> cum </p>
+        <p> cum </p>
+        <p> cum </p>
+        <p> cum </p>
+        <p> cum </p>
+    </div>
+    
+    <div class="" style="font-size: 200px; background-color: green;">
+        fart 
+        fart 
+        fart 
+        fart 
+    </div>
+
+</body>
+</html>
