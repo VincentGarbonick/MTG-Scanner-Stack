50af1678144ffc4c77adb55578965d3cc32b68ab
dt17 <jav76@uakron.edu>
2021-11-17 15:06:59 -0500

    GPIO test changes. added run_motor function and set up for subsystem demo

--- NanoControl/GPIOtest.py
+++ NanoControl/GPIOtest.py
@@ -1,25 +1,45 @@
-import nanocamera
+import nanocamera, math, time
 import Jetson.GPIO as GPIO
 
+# GLOBAL CONSTANTS
+STEP_PIN = 15
 
-import time
+# Call to send a step pulse on STEP_PIN for a specified duration at a given frequency to run a stepper motor
+def run_motor(duration = 1, freq = 2000):
+    
+    step_iterations = freq * math.floor(duration / 2)
+    period = 1 / freq
+
+    print("Running motor at", freq, "Hz for approximately", duration, "seconds.")
+
+    start_time = time.time()
+    for i in range(0, step_iterations):
+        GPIO.output(STEP_PIN, GPIO.LOW)
+        time.sleep(period)
+        GPIO.output(STEP_PIN, GPIO.HIGH)
+        time.sleep(period)
+
+    GPIO.output(STEP_PIN, GPIO.LOW)
+    end_time = time.time()
+
+    print("Runtime =", end_time - start_time, "\n")
 
 
 if __name__ == "__main__":
 
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(STEP_PIN, GPIO.OUT)
+
+
+    for j in range(1000, 4000, 500):
+        run_motor(5, j)
+        time.sleep(0.5)
+
+    print("Done")
+    GPIO.output(STEP_PIN, GPIO.LOW) 
+    GPIO.cleanup()
+    
     """
     camera = nanocamera.Camera()
     print(camera.isReady())
     """
-    GPIO.setmode(GPIO.BOARD)
-
-
-    GPIO.setup(7, GPIO.OUT)
-    for i in range(0, 500):
-        GPIO.output(7, GPIO.LOW)
-        time.sleep(0.01)
-        GPIO.output(7, GPIO.HIGH)
-        time.sleep(0.01)
-
-    print("Done")
-    GPIO.cleanup()
