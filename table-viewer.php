a2442e064111c7830b7dcd5126c623e1c8ed31d3
Vincent Garbonick <vrg6@uakron.edu>
2021-11-17 14:43:31 -0500

    add cormorant

--- table-viewer.php
+++ table-viewer.php
@@ -6,6 +6,10 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
   <link rel="shortcut icon" type="image/jpg" href="origin-symbol.png"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Cormorant:wght@500&display=swap" rel="stylesheet">
+
   <title>Table Viewer</title>
 </head>
 <body>
