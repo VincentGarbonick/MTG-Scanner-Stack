2c647070c0d229cb28cc062007b1c22edba3f470
Jaret <jav76@uakron.edu>
2022-01-17 21:38:06 -0500

    Add description for TesseractOCR/main.py

--- ImageProcessing/TesseractOCR/main.py
+++ ImageProcessing/TesseractOCR/main.py
@@ -1,10 +1,16 @@
 import pytesseract
 from PIL import Image
 
+"""
+This will be the main file for handling image processing on the Jetson Nano.
+Images will be acquired from a separate program that will be in control of motor and camera.
+This program will save captured pictures to the 'ImageTemp' directory.
+Images in this directory will then be processed as a FIFO queue. 
+Processed images will be added to the database. 
 
+"""
 
 
 
 if __name__ == "__main__":
 
-    
